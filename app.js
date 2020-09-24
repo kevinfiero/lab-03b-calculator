723880c6383503b3dcffaded8fe2a45848617b69
kevinfiero <kjf46@cornell.edu>
2020-09-24 11:09:16 -0700

    drop down operators added

--- app.js
+++ app.js
@@ -4,15 +4,36 @@ const firstVal = document.getElementById('first');
 const secondVal = document.getElementById('second');
 const calcVal = document.getElementById('calculate');
 const outputVal = document.getElementById('output');
+const operatorVal = document.getElementById('operator');
 
 // set event listeners to update state and DOM
 
 calcVal.addEventListener('click', () => {  
-     
-    const result = Number(firstVal.value) + Number(secondVal.value);
 
-    if(!result){
-        outputVal.textContent = "ERROR!" 
+    let result;
+    const indexVal = operatorVal.options[operatorVal.selectedIndex].value;
+
+    console.log(indexVal);
+
+     if(Number(indexVal)===0) {
+
+        result = Number(firstVal.value) + Number(secondVal.value);
+
+     } else if (Number(indexVal)===1) {
+
+        result = Number(firstVal.value) - Number(secondVal.value);
+
+     } else if (Number(indexVal)===2) {
+
+        result = Number(firstVal.value) * Number(secondVal.value);
+
+     } else if (Number(indexVal)===3) {
+
+        result = Number(firstVal.value) / Number(secondVal.value);
+     }
+
+    if(isNaN(result)){
+        outputVal.textContent = 'ERROR!';
     } else{
         outputVal.textContent = result;
     }
